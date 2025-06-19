4a2e44f2147a637f01e57c5fa041dd777828e166
puneethzop <puneethkumar.ht@zop.dev>
2025-06-19 10:58:29 +0530

    update unit test

--- day-3/main_test.go
+++ day-3/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -12,13 +13,15 @@ func TestAddAndCompleteTask(t *testing.T) {
 	AddTask("Write unit tests", &tasks, getNextID)
 
 	if len(tasks) != 1 {
-		t.Fatalf("Expected 1 task, got %d", len(tasks))
+		t.Errorf("Expected 1 task, got %d", len(tasks))
 	}
 	if tasks[0].Description != "Write unit tests" {
 		t.Errorf("Expected 'Write unit tests', got '%s'", tasks[0].Description)
 	}
 	if tasks[0].Completed {
 		t.Error("Expected task to be incomplete")
+	} else {
+		fmt.Println("unit test completed for Add task")
 	}
 
 	// Test CompleteTask
@@ -26,5 +29,7 @@ func TestAddAndCompleteTask(t *testing.T) {
 
 	if !tasks[0].Completed {
 		t.Error("Expected task to be completed")
+	} else {
+		fmt.Println("unit test completed for Complete task")
 	}
 }
